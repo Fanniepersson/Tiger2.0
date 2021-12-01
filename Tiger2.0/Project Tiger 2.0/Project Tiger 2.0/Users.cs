047e045f2910b853db12ea13173df2d575424de0
Danne24 <91290820+Danne24@users.noreply.github.com>
2021-12-01 16:07:31 +0100

    Add files via upload
    test

---
+++ Tiger2.0/Project Tiger 2.0/Project Tiger 2.0/Users.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_Tiger_2._0
+{
+    public class Users : BankUsers
+    {
+       public List<BankAccounts> listOfBankAccounts = new List<BankAccounts>();
+    }
+}
