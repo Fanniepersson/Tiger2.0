905539069ff4bee204de2ceb7ecca19c15d8d6fa
Daniel Gyrén <Daniel.Gyren@outlook.com>
2021-12-06 16:34:02 +0100

    Revert "added new functionality"
    This reverts commit 58413056ad7fdd18d736ed714863bff09d71f68c.

---
+++ Tiger2.0/Project Tiger 2.0/Project Tiger 2.0/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Project_Tiger_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoggingIntoBankSystem startLogin = new LoggingIntoBankSystem();
+            startLogin.LoginMenu();
+        }
+    }
+}
